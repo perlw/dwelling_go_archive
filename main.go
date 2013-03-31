cca5530a565f4ad1f9315787f9e3841d5fcf74cd
Per Loenn Wege <per.loenn.wege@gmail.com>
2013-03-31 03:18:44 +0200

    Experiment.

--- main.go
+++ main.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	"fmt"
-	gl "github.com/chsc/gogl/gl21"
+	gl "github.com/chsc/gogl/gl33"
 	"github.com/jteeuwen/glfw"
+	"io/ioutil"
 	"runtime"
+	"unsafe"
 )
 
 func main() {
-	var (
-		rotx, roty gl.Float
-	)
-
 	runtime.LockOSThread()
 
 	if err := glfw.Init(); err != nil {
@@ -20,6 +18,8 @@ func main() {
 	}
 	defer glfw.Terminate()
 
+	glfw.OpenWindowHint(glfw.OpenGLVersionMajor, 3)
+	glfw.OpenWindowHint(glfw.OpenGLVersionMinor, 3)
 	glfw.OpenWindowHint(glfw.WindowNoResize, 1)
 
 	if err := glfw.OpenWindow(640, 480, 0, 0, 0, 0, 16, 0, glfw.Windowed); err != nil {
@@ -35,43 +35,104 @@ func main() {
 		fmt.Printf("gl: %s\n", err)
 	}
 
-	//gl.Enable(gl.TEXTURE_2D)
 	gl.Enable(gl.DEPTH_TEST)
 
 	gl.ClearColor(0.5, 0.5, 0.5, 1.0)
 	gl.ClearDepth(1)
 	gl.DepthFunc(gl.LEQUAL)
 
-	gl.Viewport(0, 0, 640, 480)
-	gl.MatrixMode(gl.PROJECTION)
-	gl.LoadIdentity()
-	gl.Frustum(-1, 1, -1, 1, 1.0, 10.0)
-	gl.MatrixMode(gl.MODELVIEW)
-	gl.LoadIdentity()
+	sizeFloat := int(unsafe.Sizeof([1]float32{}))
+	sizeUint := int(unsafe.Sizeof([1]uint{}))
+	vertexData := []float32{
+		-1.0, -1.0,
+		1.0, -1.0,
+		-1.0, 1.0,
+		1.0, 1.0,
+	}
+	elementData := []uint{0, 1, 2, 3}
+	vertexBuffer := makeBuffer(gl.ARRAY_BUFFER, gl.Pointer(&vertexData[0]), sizeFloat*len(vertexData))
+	elementBuffer := makeBuffer(gl.ELEMENT_ARRAY_BUFFER, gl.Pointer(&elementData[0]), sizeUint*len(elementData))
+
+	fmt.Println(vertexBuffer)
+	fmt.Println(elementBuffer)
+
+	program := readShaders()
+	fmt.Println(program)
 
 	gl.ClearColor(0.5, 0.5, 1.0, 1.0)
 	for glfw.WindowParam(glfw.Opened) == 1 {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-		gl.MatrixMode(gl.MODELVIEW)
-		gl.LoadIdentity()
-		gl.Translated(0, 0, -3.0)
-		gl.Rotatef(rotx, 1, 0, 0)
-		gl.Rotatef(roty, 0, 1, 0)
+		gl.UseProgram(program)
 
-		rotx += 0.5
-		roty += 0.5
-
-		gl.Begin(gl.TRIANGLES)
-		gl.Normal3f(0.0, 0.0, 1.0)
-		gl.Color4f(1.0, 0.0, 0.0, 1.0)
-		gl.Vertex3f(0.0, 1.0, 0.0)
-		gl.Color4f(0.0, 1.0, 0.0, 1.0)
-		gl.Vertex3f(0.5, 0.0, 0.0)
-		gl.Color4f(0.0, 0.0, 1.0, 1.0)
-		gl.Vertex3f(-0.5, 0.0, 0.0)
-		gl.End()
+		gl.BindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
+		gl.VertexAttribPointer(0, 2, gl.FLOAT, gl.FALSE, gl.Sizei(sizeFloat*2), nil)
+		gl.EnableVertexAttribArray(0)
+		gl.DrawElements(gl.TRIANGLE_STRIP, 4, gl.UNSIGNED_SHORT, nil)
 
 		glfw.SwapBuffers()
 	}
+}
+
+func makeBuffer(target gl.Enum, buffer_data gl.Pointer, size int) gl.Uint {
+	var buffer gl.Uint
+
+	gl.GenBuffers(1, &buffer)
+	gl.BindBuffer(target, buffer)
+	gl.BufferData(target, gl.Sizeiptr(size), buffer_data, gl.STATIC_DRAW)
+
+	return buffer
+}
+
+func readShaders() gl.Uint {
+	// Vertex shader
+	vertexFile, err := ioutil.ReadFile("vertex.glsl")
+	if err != nil {
+		return 0
+	}
+	vertexSource := gl.GLString(string(vertexFile))
+	defer gl.GLStringFree(vertexSource)
+
+	vertexObj := gl.CreateShader(gl.VERTEX_SHADER)
+	gl.ShaderSource(vertexObj, 1, &vertexSource, nil)
+	gl.CompileShader(vertexObj)
+	defer gl.DeleteShader(vertexObj)
+	printShaderLog(vertexObj)
+
+	// Fragment shader
+	fragmentFile, err := ioutil.ReadFile("fragment.glsl")
+	if err != nil {
+		return 0
+	}
+	fragmentSource := gl.GLString(string(fragmentFile))
+	defer gl.GLStringFree(fragmentSource)
+
+	fragmentObj := gl.CreateShader(gl.FRAGMENT_SHADER)
+	gl.ShaderSource(fragmentObj, 1, &fragmentSource, nil)
+	gl.CompileShader(fragmentObj)
+	defer gl.DeleteShader(fragmentObj)
+	printShaderLog(fragmentObj)
+
+	// Program
+	program := gl.CreateProgram()
+	gl.AttachShader(program, vertexObj)
+	gl.AttachShader(program, fragmentObj)
+
+	// Attribs
+	fragmentOut := gl.GLString("frag")
+	defer gl.GLStringFree(fragmentOut)
+	gl.BindFragDataLocation(program, 0, fragmentOut)
+
+	gl.LinkProgram(program)
+
+	return program
+}
+
+func printShaderLog(shader gl.Uint) {
+	var length gl.Int
+	gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &length)
+	glString := gl.GLStringAlloc(gl.Sizei(length))
+	defer gl.GLStringFree(glString)
+	gl.GetShaderInfoLog(shader, gl.Sizei(length), nil, glString)
+	fmt.Println("shader log: ", gl.GoString(glString))
 }
