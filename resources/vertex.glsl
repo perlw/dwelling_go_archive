2280b5cd2534c6a7822283a40c64f89c31242f09
Per Loenn Wege <per.loenn.wege@gmail.com>
2013-04-02 21:33:21 +0200

    IT WORKS....

--- resources/vertex.glsl
+++ resources/vertex.glsl
@@ -5,8 +5,7 @@ layout(location = 0) in vec4 vert;
 uniform mat4 view;
 uniform mat4 proj;
 uniform mat4 model;
-mat4 pvm = view * proj;
 
 void main() {
-	gl_Position = view * proj * (vert * model);
+	gl_Position = proj * view * model * vert;
 }
